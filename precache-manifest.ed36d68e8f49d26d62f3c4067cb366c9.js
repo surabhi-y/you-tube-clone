5d906faccd801b6253185c221151e3f759c029d2
unknown <surabhi025yadav@gmail.com>
2020-10-23 20:49:13 +0530

    Updates

---
+++ precache-manifest.ed36d68e8f49d26d62f3c4067cb366c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc76d2e645d2bcbcf6619829520a72ff",
+    "url": "/you-tube-clone/index.html"
+  },
+  {
+    "revision": "73a079b1485598726c01",
+    "url": "/you-tube-clone/static/css/main.33600e52.chunk.css"
+  },
+  {
+    "revision": "9c11d5a59becb776cccf",
+    "url": "/you-tube-clone/static/js/2.4cf5582c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/you-tube-clone/static/js/2.4cf5582c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a079b1485598726c01",
+    "url": "/you-tube-clone/static/js/main.8f0f8368.chunk.js"
+  },
+  {
+    "revision": "dbf8a43c8abacaee2b02",
+    "url": "/you-tube-clone/static/js/runtime-main.4b473d5d.js"
+  }
+]);
